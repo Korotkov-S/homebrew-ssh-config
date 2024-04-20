427f80961a92941aca9891a5027498451e068434
Alex Korotkov <alexkorotkovh@gmail.com>
2024-04-20 09:38:14 +0300

    Init homebrew tap

---
+++ Formula/ssh-config@0.1.6.rb
@@ -0,0 +1,17 @@
+class SshConfigAT016 < Formula
+  desc "Tool for a ssh config files"
+  homepage "https://github.com/Korotkov-S/ssh-config/blob/main/Readme.md"
+  url "https://github.com/Korotkov-S/ssh-config.git", tag: "v0.1.6"
+  license ""
+  head "https://github.com/Korotkov-S/ssh-config.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "false"
+  end
+end
